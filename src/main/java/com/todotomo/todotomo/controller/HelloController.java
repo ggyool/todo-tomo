8c3cbcff011fc8e53de7ffa7befcf10e25d563b5
seunghyun <ggyool@naver.com>
2020-07-11 14:54:27 +0900

    test

--- src/main/java/com/todotomo/todotomo/controller/HelloController.java
+++ src/main/java/com/todotomo/todotomo/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.todotomo.todotomo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(){
         return "hello";
     }
